81cb1be0d8eea32c8f562a6310482299d259613b
KaduSantanaDev <kaduzera.ks@gmail.com>
2025-06-25 19:21:32 -0300

    test: ensure producer api controller is send to queue

--- apps/producer-api/src/producer-api.module.ts
+++ apps/producer-api/src/producer-api.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProducerApiController } from './producer-api.controller';
-import { ProducerApiService } from './producer-api.service';
 import { RabbitmqModule, RabbitMQService } from '@app/rabbitmq';
 
 @Module({
   imports: [RabbitmqModule],
   controllers: [ProducerApiController],
-  providers: [ProducerApiService, RabbitMQService],
+  providers: [RabbitMQService],
 })
 export class ProducerApiModule {}
