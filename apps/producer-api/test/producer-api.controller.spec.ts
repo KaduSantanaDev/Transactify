ac3b71a43ed7c041331bc9aca105f57a530fa4c7
KaduSantanaDev <kaduzera.ks@gmail.com>
2025-06-25 19:28:19 -0300

    fix: linting files

--- apps/producer-api/test/producer-api.controller.spec.ts
+++ apps/producer-api/test/producer-api.controller.spec.ts
@@ -1,45 +1,43 @@
-import { RabbitMQService } from "@app/rabbitmq"
-import { ProducerApiController } from "../src/producer-api.controller"
-import { Test, TestingModule } from "@nestjs/testing"
+import { RabbitMQService } from '@app/rabbitmq';
+import { ProducerApiController } from '../src/producer-api.controller';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('ProducerAPICOntroller', () => {
-    let controller: ProducerApiController;
-    let rabbitMQService: RabbitMQService;
+  let controller: ProducerApiController;
 
-    const mockRabbitMQService = {
-        sendToQueue: jest.fn()
-    }
+  const mockRabbitMQService = {
+    sendToQueue: jest.fn(),
+  };
 
-    beforeEach(async ()=> {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [ProducerApiController],
-            providers: [
-                {
-                    provide: RabbitMQService,
-                    useValue: mockRabbitMQService
-                }
-            ]
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProducerApiController],
+      providers: [
+        {
+          provide: RabbitMQService,
+          useValue: mockRabbitMQService,
+        },
+      ],
+    }).compile();
 
-        controller = module.get<ProducerApiController>(ProducerApiController);
-        rabbitMQService = module.get<RabbitMQService>(RabbitMQService)
-    });
+    controller = module.get<ProducerApiController>(ProducerApiController);
+  });
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    })
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined()
-    })
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 
-    it('should call sendToQueue with correct data and return status message', async() => {
-        const body = {amount: 100};
-        const result = await controller.createTransaction(body);
-        expect(mockRabbitMQService.sendToQueue).toHaveBeenCalledWith(
-            'transactions_queue',
-            body
-        );
-        expect(result).toEqual({ status: 'Transaction sent to queue'})
-    })
+  it('should call sendToQueue with correct data and return status message', async () => {
+    const body = { amount: 100 };
+    const result = await controller.createTransaction(body);
+    expect(mockRabbitMQService.sendToQueue).toHaveBeenCalledWith(
+      'transactions_queue',
+      body,
+    );
+    expect(result).toEqual({ status: 'Transaction sent to queue' });
+  });
 });
